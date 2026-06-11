e59024bbffe253b85d0fd6e6713072e1664251e7
Artur <a34396504@gmail.com>
2026-06-11 09:25:59 +0300

    feature/programmer2: функция findTopStudent

--- filterFailed.js
+++
@@ -1,9 +0,0 @@
-function filterFailed(data, passScore) {
-    const failed = [];
-    for (const student of data) {
-        if (student.score < passScore) {
-            failed.push(student.name);
-        }
-    }
-    return failed;
-}
