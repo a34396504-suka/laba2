e302d84c695cd57ef0493dd60952eae1e789f93a
Artur <a34396504@gmail.com>
2026-06-11 09:28:16 +0300

    feature/programmer4: функция addLetterGrade

--- calculateAverage.js
+++
@@ -1,7 +0,0 @@
-function calculateAverage(data) {
-    let sum = 0;
-    for (const student of data) {
-        sum += student.score;
-    }
-    return sum / data.length;
-}
--- findTopStudent.js
+++
@@ -1,9 +0,0 @@
-function findTopStudent(data) {
-    let top = data[0];
-    for (const student of data) {
-        if (student.score > top.score) {
-            top = student;
-        }
-    }
-    return top.name;
-}
